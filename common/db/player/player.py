a8b8500e136752ff841e6447b1e2ae48c98963bd
prafulliu <prafulliu@gmail.com>
2012-08-08 23:29:44 +0800

    update player table

--- common/db/player/player.py
+++ common/db/player/player.py
@@ -15,51 +15,95 @@ collection = mongo_conn.get_collection()
 class CPlayer():
     #创建角色
     def __init__(self, name, camp, occupation, zone, passport_id):
-		self._id              = mongo_conn.get_aut_inc_pk().get_pk_by_name(col_name)
-		self.passport_id      = passport_id
-		self.occupation       = occupation
-		self.create_time      = time.time()
-		self.last_login_time  = 0
-		self.last_login_map   = 0
-		self.zone             = 0
-		self.cur_mapid        = 0
-		self.x                = 0
-		self.y                = 0
-		self.name             = name
+		self._id                     = mongo_conn.get_aut_inc_pk().get_pk_by_name(col_name)
+		self.passport_id             = passport_id
+		self.occupation              = occupation
+		self.create_time             = time.time()
+		self.last_login_time         = 0
+		self.last_login_map          = 0
+		self.zone                    = 0
+		self.mapid                   = 0
+		self.x                       = 0
+		self.y                       = 0
+		self.name                    = name
 
-        #self.sex              = 0 
-        #self.head_cg          = 0 
-        #self.look             = 0 
-        #以上三项可以通过查CSV表实现
-		self.camp             = camp
-		self.lv               = 1
-		self.exp              = 0
-		self.exp_max          = 0
-		self.sp               = 0
-		self.sp_max           = 0
-		self.title            = []
-		self.title_now        = 0
-		self.silver_pack      = 0
-		self.silver_warehouse = 0
-		self.gold_pack        = 0
-		self.gold_warehouse   = 0
-		self.jade_pack        = 0
-		self.jade_warehouse   = 0
-		self.prepaid_score    = 0
-		self.activity_score   = 0
-		self.achieve_score    = 0
-		self.guild_score      = 0
+		self.camp                    = camp
+		self.lv                      = 1
+		self.exp                     = 0
+		self.exp_max                 = 0
+		self.sp                      = 0
+		self.sp_max                  = 0
+		self.title                   = []
+		self.title_now               = 0
+		self.silver_pack             = 0
+		self.silver_warehouse        = 0
+		self.gold_pack               = 0
+		self.gold_warehouse          = 0
+		self.jade_pack               = 0
+		self.jade_warehouse          = 0
+		self.prepaid_score           = 0
+		self.activity_score          = 0
+		self.achieve_score           = 0
+		self.guild_score             = 0
+        
+        
+		self.str                     = 0
+		self.str_potential           = 0
+		self.str_add                 = 0
+		self.str_max                 = 0
+
+		self.int                     = 0
+		self.int_potential           = 0
+		self.int_add                 = 0
+		self.int_max                 = 0
+
+		self.phy                     = 0
+		self.phy_potential           = 0
+		self.phy_add                 = 0
+		self.phy_max                 = 0
+
+		self.agi                     = 0
+		self.agi_potential           = 0
+		self.agi_add                 = 0
+		self.agi_max                 = 0
+
+		self.potential               = 0
+
+		self.physical_dmg            = 0
+		self.magic_dmg               = 0
+		self.physical_def            = 0
+		self.magic_def               = 0
+
+		self.hp                      = 0
+		self.hp_max                  = 0
+		self.mp                      = 0
+		self.mp_max                  = 0
+		self.acc                     = 0
+		self.miss                    = 0
+		self.cri                     = 0
+		self.tenacity                = 0
+		self.slowdown_def            = 0
+		self.vertigo_def             = 0
+		self.freeze_def              = 0
+		self.att_speed               = 0
+		self.move_speed              = 0
+		self.warfare                 = 0
+		self.sunder_armor            = 0
+		self.super_armor             = 0
+		self.rigidity                = 0
+		self.rig_def                 = 0
+		self.energy                  = 0
+
 		collection.insert(vars(self))
 
 def check_player(zone, passport_id, sitekey, sign):
     retVal = {}
-    print zone
-    print playerconfig.ZONE
     if zone in playerconfig.ZONE:
         player = collection.find_one({"passport_id":passport_id, "zone":zone})
         value = {}
         if player:
-            value['player'] = player
+            player_info = get_player_info(player)
+            value['player'] = player_info
             value['create'] = True
         else:
             value['create'] = False
@@ -131,6 +175,36 @@ def check_passport_id(passport_id):
             result = True
     return result
 
+def get_map_name(mapid):
+    map_name = ''
+    return map_name 
+
+def get_player_look(playerid):
+    look = []
+    return look
+
+def get_title_name(titleid):
+    title_name = ''
+    return title_name
+
+def get_player_info(player):
+    player_info = {}
+    if player:
+        player_info['playerid']   = player['_id']
+        player_info['mapid']      = player['mapid']
+        player_info['map_name']   = get_map_name(player['mapid'])
+        player_info['x']          = player['x']
+        player_info['y']          = player['y']
+        player_info['name']       = player['name']
+        player_info['look']       = get_player_look(player['_id'])
+        player_info['occupation'] = player['occupation']
+        player_info['title_now']  = get_title_name(player['title_now'])
+        player_info['hp']         = player['hp']
+        player_info['hp_max']     = player['hp_max']
+        player_info['mp']         = player['mp']
+        player_info['mp_max']     = player['mp_max']
+    return player_info
+
 def creat_player(name, camp, occupation, zone, passport_id):
     #创建角色
     retVal = {}
@@ -140,7 +214,10 @@ def creat_player(name, camp, occupation, zone, passport_id):
                 if check_zone(zone):
                     if check_passport_id(passport_id):
                         p = CPlayer(name, camp, occupation, zone, passport_id)
-                        retVal['value'] = vars(p)
+                        player_info = get_player_info(vars(p))
+                        value = {}
+                        value['player'] = player_info
+                        retVal['value'] = value
                         result = 0 
                     else:
                         result = 5
@@ -156,11 +233,13 @@ def creat_player(name, camp, occupation, zone, passport_id):
     return retVal
 
 if __name__ == "__main__":
-    name          = 'johnx'
+    name          = 'lily2'
     camp          = 1
     occupation    = 2
-    zone          = 1
-    passport_id   = '4'
+    zone          = 0
+    passport_id   = '22'
+    sitekey       = 0
+    sign          = 0   
     #p = CPlayer(passport, name, camp, occupation)
     #r = checkPlayer(0, '1', 0, 0)
     #print r
@@ -169,3 +248,6 @@ if __name__ == "__main__":
     
     r = creat_player(name, camp, occupation, zone, passport_id)
     print r
+   # r = check_player(zone, passport_id, sitekey, sign)
+    #print r
+    
